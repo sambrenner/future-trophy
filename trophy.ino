442c17c7523d9b8e3e9bd940e66487daddc7ae17
Sam Brenner <samjbrenner@gmail.com>
2015-03-04 23:50:15 -0500

    better handling of message buffer. begin handling cron messages

--- trophy.ino
+++ trophy.ino
@@ -5,48 +5,121 @@
 
 //arduino pins
 #define	MAX_DEVICES 5
-#define	CLK_PIN 10
-#define	DATA_PIN 8
-#define	CS_PIN 9
+#define	CLK_PIN 8
+#define	DATA_PIN 3
+#define	CS_PIN 12
 
 //parola
+#define	BUF_SIZE 75
 MD_Parola scroller = MD_Parola(DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
-MD_Parola::textEffect_t	scrollEffect = MD_Parola::SCROLL_LEFT;
-uint8_t	frameDelay = 100;
+uint8_t	frameDelay = 40;
+char nextMessage[BUF_SIZE];
+
+//arduino
+boolean contactWithLinino = false;
+boolean bridgeBegan = false;
+boolean firstRun = true;
+
+/*  linino-atmega communication
+    we listen to linino for "special strings": 
+    %%% signals that the linino has booted
+    %%! comes from a cron job on every hour, we will run the trophy at this point */
+char lininoChars[4] = "000";
 
 void setup() {
-  initScroller();
-  Bridge.begin();
-  scrollNextMessage();
-}
+  Serial1.begin(250000);
 
-void loop() {
-  if (scroller.displayAnimate()) {
-    scrollNextMessage();
-  }
-}
-
-void initScroller() {
   scroller.begin();
   scroller.displayClear();
   scroller.displaySuspend(false);
-  
-  scroller.displayScroll("loading...", MD_Parola::LEFT, scrollEffect, frameDelay);
 }
 
-void scrollNextMessage() {
-  Process process;
-  String nextMessage = "";
-
-  process.runShellCommand("python /root/trophy/trophy.py nextmsg");
-  while(process.running());
-
-  while(process.available()) {
-    nextMessage += (char)process.read();
+void loop() {
+  //read serial buffer
+  while (Serial1.available() > 0) {
+    char c = (char)Serial1.read();
+    bufferLininoCommunication(c);
   }
   
-  char msg[nextMessage.length()];
-  nextMessage.toCharArray(msg, nextMessage.length() - 1);
+  //only run when scroller is ready
+  if (scroller.displayAnimate()){
+    
+    //if we are still waiting for linino to boot:
+    if (!contactWithLinino) {
+      // check boot complete
+      if(hasLininoSaid("%%%") && !contactWithLinino){
+        contactWithLinino = true;
+      } else {
+        displayMessage("Loading...");
+      }
+    } 
+    
+    //linino has booted.
+    else {
+      //have we initialized contact with Bridge?
+      if (!bridgeBegan) {
+        Bridge.begin();
+        bridgeBegan = true;
+      }
+      
+      if(firstRun || hasLininoSaid("%%!")) {
+        bufferLininoCommunication('0');
+        firstRun = false;
+        
+        scrollNextTrophyMessage();
+      } else {
+        // hang out
+        delay(10000);
+      }
+    }
+  }
+}
+
+void scrollNextTrophyMessage() {
+  Process process;
+  String pyMessage = "";
+
+  process.runShellCommand("python /root/trophy/trophy.py nextmsg");
   
-  scroller.displayScroll(msg, MD_Parola::LEFT, scrollEffect, frameDelay);
+  while(process.running()) {}
+
+  while(process.available()) {
+    char next = process.read();
+
+    if (next != '\n' && next != '\r') {
+      pyMessage += next;
+    }
+  }
+
+  char *msg = new char[pyMessage.length() + 1];
+  strcpy(msg, pyMessage.c_str());
+  
+  displayMessage(msg);
+  
+  delete [] msg;
+}
+
+void displayMessage(char* msg) {
+  strcpy(nextMessage, msg);
+  scroller.displayScroll(nextMessage, MD_Parola::LEFT, MD_Parola::SCROLL_LEFT, frameDelay);
+}
+
+void bufferLininoCommunication(char nextChar) {
+  // store the last three chars we've received over serial from linino
+  lininoChars[0] = lininoChars[1];
+  lininoChars[1] = lininoChars[2];
+  lininoChars[2] = nextChar;
+}
+
+// http://stackoverflow.com/questions/20169474/char-array-comparison-in-c
+short hasLininoSaid(char sequence[]) {
+  int i;
+  
+  for(i = 0; i < strlen(lininoChars); i++) {
+    if(lininoChars[i] != sequence[i]) {
+      return 0;
+    }
+  }
+  
+  return 1;
 }
